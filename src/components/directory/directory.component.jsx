a0502e279e5857e42130e89073b39592562e6c47
User 1 <abdelkader@MacBook-87990.local>
2023-05-12 10:08:35 -0400

    Adjusted routing for each category to corresponding page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,40 @@
 import DirectoryItem from "../directory-item/directory-item.component";
 import "./directory.styles.scss";
 
-const Directory = ({ categories }) => {
+const categories = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/FDsy8QW/hat.webp",
+    route: "shop/hats",
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route: "shop/jackets",
+  },
+  {
+    id: 3,
+    title: "footwear",
+    imageUrl: "https://i.ibb.co/F8CsXBY/sneakers.webp",
+    route: "shop/sneakers",
+  },
+  {
+    id: 4,
+    title: "women",
+    imageUrl: "https://i.ibb.co/Vw9VnSN/woman.jpg",
+    route: "shop/womens",
+  },
+  {
+    id: 5,
+    title: "men",
+    imageUrl: "https://i.ibb.co/wy1y2Yg/men.webp",
+    route: "shop/mens",
+  },
+];
+
+const Directory = () => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./sign-in-form.styles.scss";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import FormInput from "../form-input/form-input.component";
 import { UserContext } from "../../contexts/user.contexts";
 import {
@@ -62,7 +62,11 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={SignInWithGoogle}>
+          <Button
+            type="button"
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            onClick={SignInWithGoogle}
+          >
             Google Sign In
           </Button>
         </div>
